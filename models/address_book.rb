8d7d2c7a34cd232d9c92c11577de57de5c971193
Otexas <marcellus.spears@gmail.com>
2017-07-08 08:50:25 -0500

    Assignment 4 models: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -11,7 +11,7 @@
     def add_entry(name, phone_number, email)
       # #9
       index = 0
-      entries.each do |entry|
+      @entries.each do |entry|
       # #10
         if name < entry.name
           break
@@ -21,4 +21,17 @@
       # #11
       entries.insert(index, Entry.new(name, phone_number, email))
     end
+
+    def remove_entry(name, phone_number, email)
+      delete_entry = nil
+
+      @entries.each do |entry|
+        if name == entry.name && phone_number == entry.phone_number && email == entry.email
+          delete_entry = entry
+        end
+      end
+
+      @entries.delete(delete_entry)
+    end
+
   end
