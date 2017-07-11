344f115b89555900918b7c79a7c23680ef4ef8be
Otexas <marcellus.spears@gmail.com>
2017-07-11 11:14:34 -0500

    Checkpoint 6 reading csv: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,6 @@
 # #8
   require_relative './entry.rb'
+  require "csv"
 
     class AddressBook
       attr_reader :entries
@@ -20,5 +21,17 @@
       end
       # #11
       entries.insert(index, Entry.new(name, phone_number, email))
+    end
+
+    # #7
+    def import_from_csv(file_name)
+      # Implementation goes here
+      csv_text = File.read(file_name)
+      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+      # #8
+      csv.each do |row|
+        row_hash = row.to_hash
+        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+      end
     end
   end
